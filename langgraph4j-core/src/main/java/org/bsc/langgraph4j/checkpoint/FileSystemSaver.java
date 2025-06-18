611bbe490d64fdf6081fd3dc999a9577850bc112
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-06-18 17:25:01 +0200

    refactor(core): enhance MemorySaver to become a base cache implementation for further CheckpointSaver extensions

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/checkpoint/FileSystemSaver.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/checkpoint/FileSystemSaver.java
@@ -28,151 +28,91 @@ import static java.lang.String.format;
  * </p>
  *
  */
-public class FileSystemSaver implements BaseCheckpointSaver {
+public class FileSystemSaver extends MemorySaver {
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(FileSystemSaver.class);
     public static final String EXTENSION = ".saver";
 
     private final Path targetFolder;
     private final Serializer<Checkpoint> serializer;
-    private final MemorySaver memorySaver = new MemorySaver();
 
     @SuppressWarnings("unchecked")
-    public FileSystemSaver( Path targetFolder, StateSerializer<? extends AgentState> stateSerializer) {
-        Objects.requireNonNull( stateSerializer, "stateSerializer cannot be null");
+    public FileSystemSaver(Path targetFolder, StateSerializer<? extends AgentState> stateSerializer) {
+
+        Objects.requireNonNull(stateSerializer, "stateSerializer cannot be null");
         this.targetFolder = Objects.requireNonNull(targetFolder, "targetFolder cannot be null");
-        this.serializer = new CheckPointSerializer( (StateSerializer<AgentState>) stateSerializer );
+        this.serializer = new CheckPointSerializer((StateSerializer<AgentState>) stateSerializer);
 
         File targetFolderAsFile = targetFolder.toFile();
 
-        if( targetFolderAsFile.exists() ) {
+        if (targetFolderAsFile.exists()) {
             if (targetFolderAsFile.isFile()) {
-                throw new IllegalArgumentException( format("targetFolder '%s' must be a folder", targetFolder) ); // TODO: format"targetFolder must be a directory");
+                throw new IllegalArgumentException(format("targetFolder '%s' must be a folder", targetFolder)); // TODO: format"targetFolder must be a directory");
             }
-        }
-        else {
-            if( !targetFolderAsFile.mkdirs() ) {
-                throw new IllegalArgumentException( format("targetFolder '%s' cannot be created", targetFolder) ); // TODO: format"targetFolder cannot be created");
+        } else {
+            if (!targetFolderAsFile.mkdirs()) {
+                throw new IllegalArgumentException(format("targetFolder '%s' cannot be created", targetFolder)); // TODO: format"targetFolder cannot be created");
             }
         }
 
     }
 
-    private String getBaseName( RunnableConfig config ) {
-        var threadId = config.threadId().orElse( THREAD_ID_DEFAULT );
-        return format( "thread-%s", threadId );
+    private String getBaseName(RunnableConfig config) {
+        var threadId = config.threadId().orElse(THREAD_ID_DEFAULT);
+        return format("thread-%s", threadId);
     }
 
     private Path getPath(RunnableConfig config) {
-        return Paths.get( targetFolder.toString(), getBaseName( config ).concat(EXTENSION) );
+        return Paths.get(targetFolder.toString(), getBaseName(config).concat(EXTENSION));
     }
 
     private File getFile(RunnableConfig config) {
         return getPath(config).toFile();
     }
 
-    private void serialize(LinkedList<Checkpoint> checkpoints, File outFile ) throws IOException {
-        Objects.requireNonNull( checkpoints, "checkpoints cannot be null");
-        Objects.requireNonNull( outFile, "outFile cannot be null");
-        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(outFile.toPath())) ) {
+    private void serialize(LinkedList<Checkpoint> checkpoints, File outFile) throws IOException {
+        Objects.requireNonNull(checkpoints, "checkpoints cannot be null");
+        Objects.requireNonNull(outFile, "outFile cannot be null");
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(outFile.toPath()))) {
 
-            oos.writeInt( checkpoints.size() );
-            for(Checkpoint checkpoint : checkpoints) {
+            oos.writeInt(checkpoints.size());
+            for (Checkpoint checkpoint : checkpoints) {
                 serializer.write(checkpoint, oos);
             }
         }
     }
 
-    private void deserialize( File file, LinkedList<Checkpoint> result) throws IOException, ClassNotFoundException {
-        Objects.requireNonNull(file,"file cannot be null");
-        Objects.requireNonNull(result,"result cannot be null");
+    private void deserialize(File file, LinkedList<Checkpoint> result) throws IOException, ClassNotFoundException {
+        Objects.requireNonNull(file, "file cannot be null");
+        Objects.requireNonNull(result, "result cannot be null");
 
-        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(file.toPath())) ) {
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(file.toPath()))) {
             int size = ois.readInt();
-            for( int i = 0; i < size; i++ ) {
-                result.add( serializer.read(ois) );
+            for (int i = 0; i < size; i++) {
+                result.add(serializer.read(ois));
             }
         }
     }
 
-    protected LinkedList<Checkpoint> getCheckpoints(RunnableConfig config) {
-        LinkedList<Checkpoint> result = memorySaver.getCheckpoints(config);
+    @Override
+    protected LinkedList<Checkpoint> loadedCheckpoints(RunnableConfig config, LinkedList<Checkpoint> checkpoints) throws Exception {
 
         File targetFile = getFile(config);
-        if( targetFile.exists() && result.isEmpty() ) {
-            try {
-                deserialize( targetFile, result );
-            }
-            catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+        if (targetFile.exists() && checkpoints.isEmpty()) {
+            deserialize(targetFile, checkpoints);
         }
-        return result;
-    }
+        return checkpoints;
 
-    /**
-     * Clears the checkpoint file associated with the given RunnableConfig.
-     *
-     * @param config the RunnableConfig for which the checkpoint file should be cleared
-     * @return true if the file existed and was successfully deleted, false otherwise
-     */
-    public boolean clear(RunnableConfig config) {
-        File targetFile = getFile(config);
-        return targetFile.exists() && targetFile.delete();
     }
 
     @Override
-    public Collection<Checkpoint> list(RunnableConfig config) {
-        return memorySaver.list(config);
-    }
-
-    @Override
-    public Optional<Checkpoint> get(RunnableConfig config) {
-        return memorySaver.get(config);
-    }
-
-    @Override
-    public RunnableConfig put(RunnableConfig config, Checkpoint checkpoint) throws Exception {
-        RunnableConfig result = memorySaver.put(config, checkpoint);
-
+    protected void insertedCheckpoint(RunnableConfig config, LinkedList<Checkpoint> checkpoints, Checkpoint checkpoint) throws Exception {
         File targetFile = getFile(config);
-        serialize( memorySaver.getCheckpoints(config), targetFile );
-        return result;
+        serialize(checkpoints, targetFile);
     }
 
-    private boolean createVersionedBackup( RunnableConfig config ) throws IOException {
-        Path currentPath = getPath(config);
-
-        if( !Files.exists(currentPath) ) {
-            log.warn( "file {} doesn't exist. Skipping file operations.", currentPath );
-            return false;
-        }
-
-        var versionPattern = Pattern.compile( format( "%s-v(\\d+)\\%s$", getBaseName(config), EXTENSION));
-
-        int maxVersion = 0;
-        try (var stream = Files.list(targetFolder) ) {
-            maxVersion = stream
-                    .map(path -> path.getFileName().toString())
-                    .map(versionPattern::matcher)
-                    .filter(Matcher::matches)
-                    .mapToInt(matcher -> Integer.parseInt(matcher.group(1)))
-                    .max()
-                    .orElse(0); // Default to 0 if no versioned files found
-        } catch (IOException e) {
-            log.error("Failed to list directory {} to determine next version number for backup. Skipping file operations.", targetFolder, e);
-            return false;
-        }
-
-        int nextVersion = maxVersion + 1;
-        var backupFilename = format("%s-v%d%s", getBaseName(config), nextVersion, EXTENSION);
-        Path backupPath = targetFolder.resolve(backupFilename);
-
-        Files.copy(currentPath, backupPath, StandardCopyOption.REPLACE_EXISTING);
-
-        Files.delete(currentPath);
-
-        return true;
-
+    @Override
+    protected void updatedCheckpoint(RunnableConfig config, LinkedList<Checkpoint> checkpoints, Checkpoint checkpoint) throws Exception {
+        insertedCheckpoint(config, checkpoints, checkpoint);
     }
 
     /**
@@ -183,15 +123,54 @@ public class FileSystemSaver implements BaseCheckpointSaver {
      * and then clearing the in-memory checkpoints.
      *
      * @param config The configuration for which to release checkpoints.
-     * @return The Tag representing the released checkpoint state in memory.
+     * @param checkpoints released checkpoints
+     * @param releaseTag released Tag
      * @throws Exception If an error occurs during file operations or releasing from memory.
      */
     @Override
-    public Tag release(RunnableConfig config) throws Exception {
+    protected void releasedCheckpoints(RunnableConfig config, LinkedList<Checkpoint> checkpoints, Tag releaseTag) throws Exception {
+        var currentPath = getPath(config);
 
-        createVersionedBackup(config);
+        if (!Files.exists(currentPath)) {
+            log.warn("file {} doesn't exist. Skipping file operations.", currentPath);
+            return;
+        }
 
-        return memorySaver.release(config);
+        var versionPattern = Pattern.compile(format("%s-v(\\d+)\\%s$", getBaseName(config), EXTENSION));
+
+        int maxVersion = 0;
+        try (var stream = Files.list(targetFolder)) {
+            maxVersion = stream
+                    .map(path -> path.getFileName().toString())
+                    .map(versionPattern::matcher)
+                    .filter(Matcher::matches)
+                    .mapToInt(matcher -> Integer.parseInt(matcher.group(1)))
+                    .max()
+                    .orElse(0); // Default to 0 if no versioned files found
+        } catch (IOException e) {
+            log.error("Failed to list directory {} to determine next version number for backup. Skipping file operations.", targetFolder, e);
+            return;
+        }
+
+        int nextVersion = maxVersion + 1;
+        var backupFilename = format("%s-v%d%s", getBaseName(config), nextVersion, EXTENSION);
+        Path backupPath = targetFolder.resolve(backupFilename);
+
+        Files.copy(currentPath, backupPath, StandardCopyOption.REPLACE_EXISTING);
+
+        Files.delete(currentPath);
+
+    }
+
+    /**
+     * delete the checkpoint file associated with the given RunnableConfig.
+     *
+     * @param config the RunnableConfig for which the checkpoint file should be cleared
+     * @return true if the file existed and was successfully deleted, false otherwise
+     */
+    public boolean deleteFile(RunnableConfig config) {
+        File targetFile = getFile(config);
+        return targetFile.exists() && targetFile.delete();
     }
 }
 
