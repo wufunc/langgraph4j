caef4159282526f31df73c9d259bc2d9d27e47ea
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-06-18 17:29:36 +0200

    refactor(Serializer): update method names and documentation
    - Renamed `mimeType` to `contentType` for clarification.
    - Deprecated old methods (`writeObject`, `readObject`) in favor of new ones (`objectToBytes`, `bytesToObject`).

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/serializer/Serializer.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/serializer/Serializer.java
@@ -8,11 +8,11 @@ public interface Serializer<T> {
     void write(T object, ObjectOutput out) throws IOException;
     T read(ObjectInput in) throws IOException, ClassNotFoundException;
 
-    default String mimeType() {
+    default String contentType() {
         return "application/octet-stream";
     }
 
-    default byte[] writeObject(T object) throws IOException {
+    default byte[] objectToBytes(T object) throws IOException {
         Objects.requireNonNull( object, "object cannot be null" );
         try( ByteArrayOutputStream stream = new ByteArrayOutputStream() ) {
             ObjectOutputStream oas = new ObjectOutputStream(stream);
@@ -22,7 +22,7 @@ public interface Serializer<T> {
         }
     }
 
-    default T readObject(byte[] bytes) throws IOException, ClassNotFoundException {
+    default T bytesToObject(byte[] bytes) throws IOException, ClassNotFoundException {
         Objects.requireNonNull( bytes, "bytes cannot be null" );
         if( bytes.length == 0 ) {
             throw new IllegalArgumentException("bytes cannot be empty");
@@ -31,6 +31,17 @@ public interface Serializer<T> {
             ObjectInputStream ois = new ObjectInputStream(stream);
             return read(ois);
         }
+    }
+
+
+    @Deprecated(forRemoval = true)
+    default byte[] writeObject(T object) throws IOException {
+        return objectToBytes(object);
+    }
+
+    @Deprecated(forRemoval = true)
+    default T readObject(byte[] bytes) throws IOException, ClassNotFoundException {
+        return bytesToObject(bytes);
     }
 
     default T cloneObject(T object) throws IOException, ClassNotFoundException {
