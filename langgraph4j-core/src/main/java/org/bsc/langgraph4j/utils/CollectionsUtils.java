4b43be82dba954f671273d7750a6465a7d26a43f
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-07-10 17:09:15 +0200

    feat(core): add toString utility methods for collection and map
    Added utility methods `toString` to improve string representation of collections and maps, enhancing readability and usability in debugging and logging scenarios.

--- langgraph4j-core/src/main/java/org/bsc/langgraph4j/utils/CollectionsUtils.java
+++ langgraph4j-core/src/main/java/org/bsc/langgraph4j/utils/CollectionsUtils.java
@@ -13,6 +13,31 @@ import static java.util.Collections.unmodifiableMap;
  */
 public final class CollectionsUtils {
 
+    public static String toString( Collection<?> collection ) {
+        if(collection.isEmpty()) {
+            return "[]";
+        }
+        return collection.stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining("\n\t", "[\n\t", "\n\t]"));
+
+    }
+
+    public static String toString( Map<?,?> map ) {
+        if(map.isEmpty()) {
+            return "{}";
+        }
+        return map.entrySet().stream()
+                .map( entry -> {
+                    if( entry.getValue() instanceof Collection<?> elements ) {
+                        return String.format("%s=%s", entry.getKey(), toString(elements));
+                    }
+                    return Objects.toString(entry);
+                })
+                .collect(Collectors.joining("\n\t", "{\n\t", "\n\t}") );
+
+    }
+
     /**
      * Returns the last value in the list, if present.
      *
