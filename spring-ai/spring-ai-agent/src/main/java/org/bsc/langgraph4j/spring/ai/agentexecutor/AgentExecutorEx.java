20242925ffd870ae80b519e740afb5a49e297a19
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-07-10 17:15:55 +0200

    feat(spring-ai-agent): allows conditional tool execution approvals (HITL), enhancing control over agent actions.
    * Introduced an ApprovalNodeAction class to handle approval logic for tool executions.
    * Updated the State class to include additional keys and methods for tracking tool execution results and next actions.
    * Modified the dispatchTools method to handle approval for specific tool actions.

--- spring-ai/spring-ai-agent/src/main/java/org/bsc/langgraph4j/spring/ai/agentexecutor/AgentExecutorEx.java
+++ spring-ai/spring-ai-agent/src/main/java/org/bsc/langgraph4j/spring/ai/agentexecutor/AgentExecutorEx.java
@@ -1,12 +1,10 @@
 package org.bsc.langgraph4j.spring.ai.agentexecutor;
 
 import org.bsc.langgraph4j.GraphStateException;
+import org.bsc.langgraph4j.RunnableConfig;
 import org.bsc.langgraph4j.StateGraph;
-import org.bsc.langgraph4j.action.AsyncNodeAction;
-import org.bsc.langgraph4j.action.AsyncNodeActionWithConfig;
-import org.bsc.langgraph4j.action.EdgeAction;
+import org.bsc.langgraph4j.action.*;
 import org.bsc.langgraph4j.prebuilt.MessagesState;
-import org.bsc.langgraph4j.spring.ai.generators.StreamingChatGenerator;
 import org.bsc.langgraph4j.spring.ai.serializer.std.SpringAIStateSerializer;
 import org.bsc.langgraph4j.spring.ai.tool.SpringAIToolService;
 import org.bsc.langgraph4j.state.AgentState;
@@ -20,10 +18,14 @@ import org.springframework.ai.chat.messages.ToolResponseMessage;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
 
+import static java.lang.String.format;
+import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.bsc.langgraph4j.StateGraph.START;
 import static org.bsc.langgraph4j.action.AsyncEdgeAction.edge_async;
-import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
+import static org.bsc.langgraph4j.state.AgentState.MARK_FOR_REMOVAL;
+import static org.bsc.langgraph4j.state.AgentState.MARK_FOR_RESET;
 import static org.bsc.langgraph4j.utils.CollectionsUtils.mapOf;
 import static org.bsc.langgraph4j.utils.CollectionsUtils.mergeMap;
 
@@ -49,83 +51,6 @@ import static org.bsc.langgraph4j.utils.CollectionsUtils.mergeMap;
 public interface AgentExecutorEx {
 
     org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AgentExecutorEx.class);
-
-    /**
-     * Class responsible for building a state graph.
-     */
-    class Builder extends AgentExecutorBuilder<Builder, State> {
-
-        /**
-         * Builds and returns a StateGraph with the specified configuration.
-         * Initializes the stateSerializer if it's null. Then, constructs a new StateGraph object using the provided schema
-         * and serializer, adds an initial edge from the START node to "agent", and then proceeds to add nodes for "agent" and
-         * "action". It also sets up conditional edges from the "agent" node based on whether or not to continue.
-         *
-         * @return A configured StateGraph object.
-         * @throws GraphStateException If there is an issue with building the graph state.
-         */
-        public StateGraph<State> build() throws GraphStateException {
-
-            if (stateSerializer == null) {
-                stateSerializer = new SpringAIStateSerializer<>(AgentExecutorEx.State::new);
-            }
-
-            var chatService = new ChatService(this);
-
-            var tools = chatService.tools();
-
-            final var toolService = new SpringAIToolService(tools);
-
-            AsyncNodeActionWithConfig<State> callModelAction = CallModel.of( chatService, streaming );
-
-            AsyncNodeAction<State> dispatchToolsAction = node_async(AgentExecutorEx::dispatchTools);
-
-            final EdgeAction<State> dispatchAction = (state) ->
-                    state.nextAction().orElse("model");
-
-            var graph = new StateGraph<>(State.SCHEMA, stateSerializer)
-                    .addNode("model",callModelAction )
-                    .addNode("action_dispatcher", dispatchToolsAction)
-                    .addEdge(START, "model")
-                    .addConditionalEdges("model",
-                            edge_async(AgentExecutorEx::shouldContinue),
-                            EdgeMappings.builder()
-                                    .to("action_dispatcher", "continue")
-                                    .toEND("end" )
-                                    .build()) ;
-
-            var actionMappingBuilder  =  EdgeMappings.builder()
-                    .to( "model")
-                    .toEND();
-
-            for (var tool : tools) {
-
-                var tool_name = tool.getToolDefinition().name();
-
-                actionMappingBuilder.to(tool_name );
-
-                graph.addNode(tool_name,
-                        state -> executeTools( state, toolService, tool_name));
-                graph.addEdge(tool_name, "action_dispatcher");
-
-            }
-
-            return   graph.addConditionalEdges( "action_dispatcher",
-                    edge_async(dispatchAction),
-                    actionMappingBuilder.build())
-                    ;
-        }
-
-    }
-
-    /**
-     * Returns a new instance of {@link Builder}.
-     *
-     * @return a new {@link Builder} object
-     */
-    static Builder builder() {
-        return new Builder();
-    }
 
     /**
      * Represents the state of an agent in a system.
@@ -156,32 +81,259 @@ public interface AgentExecutorEx {
             return value("next_action");
         }
 
+        private  List<AssistantMessage.ToolCall> toolCallsByName( String actionName ) {
+            return lastMessage()
+                    .filter(m -> MessageType.ASSISTANT == m.getMessageType())
+                    .map(AssistantMessage.class::cast)
+                    .filter(AssistantMessage::hasToolCalls)
+                    .map(AssistantMessage::getToolCalls)
+                    .map(requests -> requests.stream()
+                            .filter(req -> Objects.equals(req.name(), actionName)).toList())
+                    .orElseGet(List::of)
+                    ;
+        }
     }
 
-    private static Map<String, Object> dispatchTools(State state ) {
-        log.trace( "DispatchTools" );
 
-        var toolExecutionRequests = state.lastMessage()
-                .filter( m -> MessageType.ASSISTANT==m.getMessageType() )
-                .map( AssistantMessage.class::cast )
-                .filter(AssistantMessage::hasToolCalls)
-                .map(AssistantMessage::getToolCalls);
+    enum ApprovalState {
+        APPROVED,
+        REJECTED
+    }
 
-        if( toolExecutionRequests.isEmpty() ) {
-            return Map.of("agent_response", "no tool execution request found!" );
+    final class ApprovalNodeAction implements AsyncNodeActionWithConfig<State>, InterruptableAction<State> {
+
+        private final String resumePropertyName;
+        private final BiFunction<String, State, InterruptionMetadata<State>> interruptionMetadataProvider;
+
+        private ApprovalNodeAction( Builder builder ) {
+            this.resumePropertyName = builder.resumePropertyName;
+            this.interruptionMetadataProvider = builder.interruptionMetadataProvider;
         }
 
-        var requests = toolExecutionRequests.get();
+        @Override
+        public CompletableFuture<Map<String, Object>> apply(State state, RunnableConfig config) {
+            return completedFuture(Map.of());
+        }
 
-        return requests.stream()
-                .filter( request -> state.toolExecutionResults().stream()
-                        .flatMap( r -> r.getResponses().stream() )
-                        .noneMatch( r -> Objects.equals(r.name(), request.name())))
-                .findFirst()
-                .map( result -> Map.<String,Object>of( "next_action", result.name() ))
-                .orElseGet( () ->  mapOf("messages",  state.toolExecutionResults(),
-                        "tool_execution_results", null, /* reset results */
-                        "next_action", null  /* remove element */ ));
+        @Override
+        public Optional<InterruptionMetadata<State>> interrupt(String nodeId, State state) {
+            if( state.value( resumePropertyName ).isEmpty() ) {
+                var metadata = interruptionMetadataProvider.apply(nodeId,state);
+                return Optional.of(metadata);
+            }
+            return Optional.empty();
+        }
+
+        AsyncCommandAction<State> edgeAction() {
+            return (state, config) -> {
+                var result = new CompletableFuture<Command>();
+
+                if( state.value( resumePropertyName ).isEmpty() ) {
+                    result.completeExceptionally( new IllegalStateException(format("resume property '%s' not found!", resumePropertyName) ));
+                    return result;
+                }
+
+                var resumeState = state.<String>value( resumePropertyName )
+                                    .orElseThrow( () -> new IllegalStateException(format("resume property '%s' not found!", resumePropertyName) ));
+
+                if( Objects.equals( resumeState, ApprovalState.APPROVED.name() )) {
+                    result.complete( new Command( resumeState,
+                            Map.of(resumePropertyName, MARK_FOR_REMOVAL)));
+
+                }
+                else {
+                    var actionName = state.nextAction()
+                            .map( v -> v.replace("approval_", "") )
+                            .orElseThrow( () -> new IllegalStateException("no next action found!"));
+
+                    var tools = state.toolCallsByName( actionName );
+
+                    if(tools.isEmpty())  {
+                        throw new IllegalStateException("no tool execution request found!");
+                    }
+
+                    var toolResponses = tools.stream().map( toolCall ->
+                        new ToolResponseMessage.ToolResponse(toolCall.id(), actionName, "execution has been DENIED!")
+                    ).toList();
+
+                    result.complete( new Command( resumeState,
+                            Map.of( "messages", new ToolResponseMessage( toolResponses ),
+                                    "tool_execution_results", "execution has been DENIED!",
+                                    resumePropertyName, MARK_FOR_REMOVAL)));
+
+                }
+                return result;
+            };
+        }
+        public static  Builder builder() {
+            return new Builder();
+        }
+
+        public static class Builder {
+            private String resumePropertyName;
+            private BiFunction<String, AgentExecutorEx.State, InterruptionMetadata<State>> interruptionMetadataProvider;
+
+            public Builder resumePropertyName( String name  ) {
+                resumePropertyName = name;
+                return this;
+            }
+            public Builder interruptionMetadataProvider(  BiFunction<String, State, InterruptionMetadata<State>> provider  ) {
+                interruptionMetadataProvider = provider;
+                return this;
+            }
+
+            public ApprovalNodeAction build() {
+                Objects.requireNonNull(resumePropertyName, "resumePropertyName cannot be null!");
+                Objects.requireNonNull(interruptionMetadataProvider, "interruptionMetadataProvider cannot be null!");
+                return new ApprovalNodeAction(this);
+            }
+
+        }
+    }
+
+    /**
+     * Class responsible for building a state graph.
+     */
+    class Builder extends AgentExecutorBuilder<Builder, State> {
+
+        private final Map<String,ApprovalNodeAction> approvals = new LinkedHashMap<>();
+
+        public Builder approvalOn( String actionId, ApprovalNodeAction action ) {
+            approvals.put( actionId, action  );
+            return this;
+        }
+
+        /**
+         * Builds and returns a StateGraph with the specified configuration.
+         * Initializes the stateSerializer if it's null. Then, constructs a new StateGraph object using the provided schema
+         * and serializer, adds an initial edge from the START node to "agent", and then proceeds to add nodes for "agent" and
+         * "action". It also sets up conditional edges from the "agent" node based on whether or not to continue.
+         *
+         * @return A configured StateGraph object.
+         * @throws GraphStateException If there is an issue with building the graph state.
+         */
+        public StateGraph<State> build() throws GraphStateException {
+
+            if (stateSerializer == null) {
+                stateSerializer = new SpringAIStateSerializer<>(AgentExecutorEx.State::new);
+            }
+
+            var chatService = new ChatService(this);
+
+            var tools = chatService.tools();
+
+            // verify approval
+            for (var approval : approvals.keySet()) {
+
+                tools.stream()
+                        .filter( tool -> Objects.equals( tool.getToolDefinition().name(), approval) )
+                        .findAny()
+                        .orElseThrow( () -> new IllegalArgumentException(format("approval action %s not found!", approval) ));
+            }
+
+            final var toolService = new SpringAIToolService(tools);
+
+            AsyncNodeActionWithConfig<State> callModelAction = CallModel.of( chatService, streaming );
+
+            AsyncNodeAction<State> dispatchToolsAction = dispatchTools( approvals.keySet() );
+
+            final EdgeAction<State> dispatchAction = (state) ->
+                    state.nextAction().orElse("model");
+
+            var graph = new StateGraph<>(State.SCHEMA, stateSerializer)
+                    .addNode("model",callModelAction )
+                    .addNode("action_dispatcher", dispatchToolsAction)
+                    .addEdge(START, "model")
+                    .addConditionalEdges("model",
+                            edge_async(AgentExecutorEx::shouldContinue),
+                            EdgeMappings.builder()
+                                    .to("action_dispatcher", "continue")
+                                    .toEND("end" )
+                                    .build()) ;
+
+            var actionMappingBuilder  =  EdgeMappings.builder()
+                    .to( "model")
+                    .toEND();
+
+            for (var tool : tools) {
+
+                var tool_name = tool.getToolDefinition().name();
+
+                if( approvals.containsKey(tool_name) ) {
+
+                    var approval_nodeId = format("approval_%s", tool_name);
+
+                    var approvalAction = approvals.get(tool_name);
+
+                    graph.addNode( approval_nodeId, approvalAction );
+
+                    graph.addConditionalEdges( approval_nodeId, approvalAction.edgeAction() ,
+                            EdgeMappings.builder()
+                                    .to( "model", ApprovalState.REJECTED.name())
+                                    .to( tool_name, ApprovalState.APPROVED.name() )
+                                    .build()
+                            );
+
+                    actionMappingBuilder.to(approval_nodeId);
+                }
+                else {
+                    actionMappingBuilder.to(tool_name);
+                }
+
+                graph.addNode(tool_name,
+                        state -> executeTools( state, toolService, tool_name));
+                graph.addEdge(tool_name, "action_dispatcher");
+
+            }
+
+            return   graph.addConditionalEdges( "action_dispatcher",
+                    edge_async(dispatchAction),
+                    actionMappingBuilder.build())
+                    ;
+        }
+
+    }
+
+    /**
+     * Returns a new instance of {@link Builder}.
+     *
+     * @return a new {@link Builder} object
+     */
+    static Builder builder() {
+        return new Builder();
+    }
+
+    private static AsyncNodeAction<State> dispatchTools(Set<String> approvals ) {
+
+        return AsyncNodeAction.node_async(( state ) -> {
+            log.trace( "DispatchTools" );
+
+            var toolExecutionRequests = state.lastMessage()
+                    .filter( m -> MessageType.ASSISTANT==m.getMessageType() )
+                    .map( AssistantMessage.class::cast )
+                    .filter(AssistantMessage::hasToolCalls)
+                    .map(AssistantMessage::getToolCalls);
+
+            if( toolExecutionRequests.isEmpty() ) {
+                return Map.of("agent_response", "no tool execution request found!" );
+            }
+
+            var requests = toolExecutionRequests.get();
+
+            return requests.stream()
+                    .filter( request -> state.toolExecutionResults().stream()
+                            .flatMap( r -> r.getResponses().stream() )
+                            .noneMatch( r -> Objects.equals(r.name(), request.name())))
+                    .findFirst()
+                    .map( result -> ( approvals.contains(result.name()) ?
+                            format( "approval_%s", result.name() ) :
+                            result.name()))
+                    .map( actionId -> Map.<String,Object>of( "next_action", actionId ))
+                    .orElseGet( () ->  Map.of("messages",  state.toolExecutionResults(),
+                            "tool_execution_results", MARK_FOR_RESET, /* reset results */
+                            "next_action", MARK_FOR_RESET  /* remove element */ ));
+
+        });
 
     }
     /**
@@ -193,15 +345,7 @@ public interface AgentExecutorEx {
     static CompletableFuture<Map<String, Object>> executeTools(State state, SpringAIToolService toolService, String actionName ) {
         log.trace( "ExecuteTool" );
 
-        var toolCalls = state.lastMessage()
-                .filter( m -> MessageType.ASSISTANT==m.getMessageType() )
-                .map( AssistantMessage.class::cast )
-                .filter(AssistantMessage::hasToolCalls)
-                .map(AssistantMessage::getToolCalls)
-                .map( requests -> requests.stream()
-                        .filter( req -> Objects.equals(req.name(), actionName)).toList())
-                .orElseGet(List::of)
-                ;
+        var toolCalls = state.toolCallsByName(actionName);
 
         if( toolCalls.isEmpty() ) {
             return CompletableFuture.failedFuture( new IllegalArgumentException("no tool execution request found!") );
@@ -236,3 +380,4 @@ public interface AgentExecutorEx {
         return "end";
     }
 }
+
--- spring-ai/spring-ai-agent/src/test/java/org/bsc/langgraph4j/spring/ai/agentexecutor/DemoConsoleController.java
+++ spring-ai/spring-ai-agent/src/test/java/org/bsc/langgraph4j/spring/ai/agentexecutor/DemoConsoleController.java
@@ -1,17 +1,22 @@
 package org.bsc.langgraph4j.spring.ai.agentexecutor;
 
-import org.bsc.langgraph4j.NodeOutput;
+import org.bsc.async.AsyncGenerator;
+import org.bsc.langgraph4j.CompileConfig;
+import org.bsc.langgraph4j.GraphRepresentation;
+import org.bsc.langgraph4j.RunnableConfig;
+import org.bsc.langgraph4j.action.InterruptionMetadata;
+import org.bsc.langgraph4j.checkpoint.MemorySaver;
 import org.bsc.langgraph4j.streaming.StreamingOutput;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.tool.ToolCallback;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
+import static java.lang.String.format;
 
 /**
  * Demonstrates the use of Spring Boot CLI to execute a task using an agent executor.
@@ -41,31 +46,79 @@ public class DemoConsoleController implements CommandLineRunner {
 
         log.info("Welcome to the Spring Boot CLI application!");
 
+        var saver = new MemorySaver();
+
+        var compileConfig = CompileConfig.builder()
+                                .checkpointSaver(saver)
+                                .build();
+
         var agent = AgentExecutorEx.builder()
                         //.streamingChatModel(chatModel)
                         .chatModel(chatModel)
                         .toolsFromObject( new TestTool()) // Support without providing tools
+                        .approvalOn( "execTest", AgentExecutorEx.ApprovalNodeAction.builder()
+                                                        .resumePropertyName("resume")
+                                                        .interruptionMetadataProvider( ( nodeId, state ) ->
+                                                            InterruptionMetadata.builder( nodeId, state )
+                                                                    .addMetadata( "label", "confirm execution of test?")
+                                                                    .build()
+                                                        )
+                                                        .build() )
                         .build()
-                        .compile();
+                        .compile(compileConfig);
 
-        var result = agent.stream( Map.of( "messages", new UserMessage("perform test twice") ));
+        log.info( "{}", agent.getGraph( GraphRepresentation.Type.MERMAID, "ReAct Agent", false));
 
-        var state = result.stream()
-                .peek( s -> {
-                    if( s instanceof StreamingOutput<?> sout ) {
-                        System.out.printf( "%s: (%s)\n", sout.node(), sout.chunk());
+        Map<String,Object> input = Map.of("messages", new UserMessage("perform test") );
+        var runnableConfig = RunnableConfig.builder().build();
+
+        var console = System.console();
+
+        while( true ) {
+            var result = agent.stream(input, runnableConfig );
+
+            var output = result.stream()
+                    .peek(s -> {
+                        if (s instanceof StreamingOutput<?> out) {
+                            System.out.printf("%s: (%s)\n", out.node(), out.chunk());
+                        } else {
+                            System.out.println(s.node());
+                        }
+                    })
+                    .reduce((a, b) -> b)
+                    .orElseThrow();
+
+            if (output.isEND()) {
+                console.format( "result: %s\n",
+                        output.state().lastMessage()
+                        .map(AssistantMessage.class::cast)
+                        .map(AssistantMessage::getText)
+                        .orElseThrow());
+                break;
+
+            } else {
+
+                var returnValue = AsyncGenerator.resultValue(result);
+
+                if( returnValue.isPresent() ) {
+
+                    log.info("interrupted: {}", returnValue.orElse("NO RESULT FOUND!"));
+
+                    if (returnValue.get() instanceof InterruptionMetadata<?> interruption) {
+
+                        var answer = console.readLine(format("%s : (N\\y) \t\n", interruption.metadata("label").orElse("Approve action ?")));
+
+                        if (Objects.equals(answer, "Y") || Objects.equals(answer, "y")) {
+                            runnableConfig = agent.updateState(runnableConfig, Map.of("resume", AgentExecutorEx.ApprovalState.APPROVED.name()));
+                        } else {
+                            runnableConfig = agent.updateState(runnableConfig, Map.of("resume", AgentExecutorEx.ApprovalState.REJECTED.name()));
+                        }
                     }
-                    else {
-                        System.out.println(s.node());
-                    }
-                })
-                .reduce((a, b) -> b)
-                .map( NodeOutput::state)
-                .orElseThrow();
+                    input = null;
+                }
 
-        log.info( "result: {}", state.lastMessage()
-                                    .map(AssistantMessage.class::cast)
-                                    .map(AssistantMessage::getText)
-                                    .orElseThrow() );
+            }
+
+        }
     }
 }
