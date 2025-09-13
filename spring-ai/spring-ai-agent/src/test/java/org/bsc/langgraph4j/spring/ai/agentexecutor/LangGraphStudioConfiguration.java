47df74f1a89f0659054fd63f5b05ab2c3fb6742b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-09-13 16:40:50 +0200

    refactor(springai-agent): Refactored LangGraphStudioConfiguration to use Map<String, LangGraphStudioServer.Instance>
    work on #233

--- spring-ai/spring-ai-agent/src/test/java/org/bsc/langgraph4j/spring/ai/agentexecutor/LangGraphStudioConfiguration.java
+++ spring-ai/spring-ai-agent/src/test/java/org/bsc/langgraph4j/spring/ai/agentexecutor/LangGraphStudioConfiguration.java
@@ -6,18 +6,19 @@ import org.bsc.langgraph4j.GraphStateException;
 import org.bsc.langgraph4j.StateGraph;
 import org.bsc.langgraph4j.checkpoint.MemorySaver;
 import org.bsc.langgraph4j.state.AgentState;
-import org.bsc.langgraph4j.studio.springboot.AbstractLangGraphStudioConfig;
-import org.bsc.langgraph4j.studio.springboot.LangGraphFlow;
+import org.bsc.langgraph4j.studio.LangGraphStudioServer;
+import org.bsc.langgraph4j.studio.springboot.LangGraphStudioConfig;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
 import java.util.Objects;
 
 @Configuration
-public class LangGraphStudioConfiguration extends AbstractLangGraphStudioConfig {
+public class LangGraphStudioConfiguration extends LangGraphStudioConfig {
 
-    final LangGraphFlow flow;
+    final Map<String, LangGraphStudioServer.Instance> instanceMap;
 
     public LangGraphStudioConfiguration( /*@Qualifier("ollama")*/ ChatModel chatModel ) throws GraphStateException {
 
@@ -30,25 +31,25 @@ public class LangGraphStudioConfiguration extends AbstractLangGraphStudioConfig 
         var mermaid = workflow.getGraph( GraphRepresentation.Type.MERMAID, "ReAct Agent", false );
         System.out.println( mermaid.content() );
 
-        this.flow = agentWorkflow( workflow );
+        this.instanceMap = agentWorkflow( workflow );
     }
 
-    private LangGraphFlow agentWorkflow( StateGraph<? extends AgentState> workflow ) throws GraphStateException {
+    private Map<String, LangGraphStudioServer.Instance> agentWorkflow( StateGraph<? extends AgentState> workflow ) throws GraphStateException {
 
-        return  LangGraphFlow.builder()
-                .title("LangGraph Studio (Spring AI)")
-                .addInputStringArg( "messages", true, v -> new UserMessage( Objects.toString(v) ) )
-                .stateGraph( workflow )
-                .compileConfig( CompileConfig.builder()
-                        .checkpointSaver( new MemorySaver() )
-                        .releaseThread(true)
-                        .build())
-                .build();
+        return  Map.of( "sample", LangGraphStudioServer.Instance.builder()
+                    .title("LangGraph Studio (Spring AI)")
+                    .addInputStringArg( "messages", true, v -> new UserMessage( Objects.toString(v) ) )
+                    .graph( workflow )
+                    .compileConfig( CompileConfig.builder()
+                            .checkpointSaver( new MemorySaver() )
+                            .releaseThread(true)
+                            .build())
+                    .build());
 
     }
 
     @Override
-    public LangGraphFlow getFlow() {
-        return this.flow;
+    public Map<String, LangGraphStudioServer.Instance> instanceMap() {
+        return instanceMap;
     }
 }
